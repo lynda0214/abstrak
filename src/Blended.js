1778f854bb01a5ca768acda2e7e731ba0247e065
Lynda Lin <lynda0214@gmail.com>
2022-09-14 15:48:20 +0800

    mix with template olmo, <Blogs>

--- src/Blended.js
+++ src/Blended.js
@@ -5,10 +5,15 @@ import 'aos/dist/aos.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/scss/app.scss';
 import './assets/main.scss';
+import "./styles/globals.css";
 
 import HeaderOne from './common/header/HeaderOne';
 import About from './component/about/About.js';
 import PricingFive from './component/pricing/PricingFive';
+import Blogs from './component/blog/Blogs';
+
+const Separater = ({ title }) => <div
+  style={{ background: 'crimson', color: 'white', fontWeight: 'bold', textAlign: 'center' }}>{title}</div>;
 
 const Blended = () => {
   useEffect(() => {
@@ -20,13 +25,18 @@ const Blended = () => {
 
   return (
     <Router>
+      <Separater title="Abstrak <HeaderOne>"/>
       <HeaderOne/>
+      <Separater title="Deski <About>"/>
       <About/>
+      <Separater title="Doob <PricingFive>"/>
       <div className="rwt-pricing-area rn-section-gap">
         <div className="container">
           <PricingFive />
         </div>
       </div>
+      <Separater title="OLMO <Blogs>"/>
+      <Blogs/>
     </Router>
   );
 }
