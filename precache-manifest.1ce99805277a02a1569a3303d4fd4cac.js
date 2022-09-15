c740b9d33380f8b78fc3d345ffa1b62032f842e0
Lynda Lin <lynda0214@gmail.com>
2022-09-15 16:37:37 +0800

    Updates

--- precache-manifest.1ce99805277a02a1569a3303d4fd4cac.js
+++ precache-manifest.1ce99805277a02a1569a3303d4fd4cac.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "711138a8004307d6416f",
+    "revision": "40117f302a06d686df01",
     "url": "/abstrak/static/css/main.94d13817.chunk.css"
   },
   {
-    "revision": "711138a8004307d6416f",
-    "url": "/abstrak/static/js/main.c0dba84c.chunk.js"
+    "revision": "40117f302a06d686df01",
+    "url": "/abstrak/static/js/main.444b3bc7.chunk.js"
   },
   {
     "revision": "0b7e43c8dbf471b0e6b5",
@@ -28,28 +28,24 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/dot-bg-01.2548ec05.svg"
   },
   {
-    "revision": "3ea4f36c1d9cd45fa069313f74836838",
-    "url": "/abstrak/static/media/gilroy-black-webfont.3ea4f36c.woff2"
-  },
-  {
     "revision": "5773a94c18d8887d33466c3b06861f46",
     "url": "/abstrak/static/media/gordita_regular-webfont.5773a94c.woff2"
-  },
-  {
-    "revision": "e1cde554041f8e8c0757120f84fb5cc8",
-    "url": "/abstrak/static/media/gordita_medium-webfont.e1cde554.woff"
   },
   {
     "revision": "b348d2a338d810ab4c7ddd418085a7a1",
     "url": "/abstrak/static/media/gordita_regular-webfont.b348d2a3.woff"
   },
   {
+    "revision": "5932b270d0837f318fa083a1ad83ed43",
+    "url": "/abstrak/static/media/gilroy-black-webfont.5932b270.woff"
+  },
+  {
     "revision": "9321c60fd70333acc9034fe7f0970f94",
     "url": "/abstrak/static/media/gordita_medium-webfont.9321c60f.woff2"
   },
   {
-    "revision": "5932b270d0837f318fa083a1ad83ed43",
-    "url": "/abstrak/static/media/gilroy-black-webfont.5932b270.woff"
+    "revision": "3ea4f36c1d9cd45fa069313f74836838",
+    "url": "/abstrak/static/media/gilroy-black-webfont.3ea4f36c.woff2"
   },
   {
     "revision": "0d8e344f21cce9f34cbf05eeeece8a6b",
@@ -60,28 +56,28 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/gilroy-bold-webfont.7a72232e.woff2"
   },
   {
-    "revision": "7bc683fd9e707d8fa545e624c9c60af6",
-    "url": "/abstrak/static/media/gilroy-semibold-webfont.7bc683fd.woff"
-  },
-  {
-    "revision": "5c791464f3c0531c2bf498dca26edef5",
-    "url": "/abstrak/static/media/gilroy-light-webfont.5c791464.woff2"
-  },
-  {
-    "revision": "cb18b3754b7210eee43c4cf66ff9dc2e",
-    "url": "/abstrak/static/media/gilroy-light-webfont.cb18b375.woff"
-  },
-  {
     "revision": "e8807cdc6d60a23330fec3b57df1064e",
     "url": "/abstrak/static/media/gilroy-semibold-webfont.e8807cdc.woff2"
+  },
+  {
+    "revision": "7bc683fd9e707d8fa545e624c9c60af6",
+    "url": "/abstrak/static/media/gilroy-semibold-webfont.7bc683fd.woff"
   },
   {
     "revision": "0f2434d02704a1b29b5f5a99855143c0",
     "url": "/abstrak/static/media/gilroy-ultralight-webfont.0f2434d0.woff2"
   },
   {
+    "revision": "5c791464f3c0531c2bf498dca26edef5",
+    "url": "/abstrak/static/media/gilroy-light-webfont.5c791464.woff2"
+  },
+  {
     "revision": "aa9a7e9934f006f8364c0f0ba2470cfd",
     "url": "/abstrak/static/media/gilroy-ultralight-webfont.aa9a7e99.woff"
+  },
+  {
+    "revision": "cb18b3754b7210eee43c4cf66ff9dc2e",
+    "url": "/abstrak/static/media/gilroy-light-webfont.cb18b375.woff"
   },
   {
     "revision": "53412578ec47bb89e0b2db7e22b1e34f",
@@ -100,52 +96,52 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/Recoleta-Thin.5baa2aa6.ttf"
   },
   {
-    "revision": "87acbdc2ee2675f067847a6b6abbeb9c",
-    "url": "/abstrak/static/media/Recoleta-Light.87acbdc2.eot"
-  },
-  {
-    "revision": "6722f7d9ecf95796ddfd2193f7e42760",
-    "url": "/abstrak/static/media/Recoleta-Light.6722f7d9.woff"
-  },
-  {
     "revision": "83647f8e855102545dab71b5be6b92b7",
     "url": "/abstrak/static/media/Recoleta-Light.83647f8e.woff2"
+  },
+  {
+    "revision": "87acbdc2ee2675f067847a6b6abbeb9c",
+    "url": "/abstrak/static/media/Recoleta-Light.87acbdc2.eot"
   },
   {
     "revision": "1dd453d089bdaf8187c1d7488cb9ff32",
     "url": "/abstrak/static/media/Recoleta-Light.1dd453d0.ttf"
   },
   {
-    "revision": "6aff230c15539d9aa16c3c8cc0f6be16",
-    "url": "/abstrak/static/media/Recoleta-Regular.6aff230c.woff"
+    "revision": "6722f7d9ecf95796ddfd2193f7e42760",
+    "url": "/abstrak/static/media/Recoleta-Light.6722f7d9.woff"
   },
   {
-    "revision": "9711a8e7897d2d876b836ac797eabca8",
-    "url": "/abstrak/static/media/Recoleta-Regular.9711a8e7.eot"
-  },
-  {
-    "revision": "7eb49619205b759292e7ec0a78a61e6c",
-    "url": "/abstrak/static/media/Recoleta-Regular.7eb49619.ttf"
+    "revision": "e1cde554041f8e8c0757120f84fb5cc8",
+    "url": "/abstrak/static/media/gordita_medium-webfont.e1cde554.woff"
   },
   {
     "revision": "f6e3b6a90d26ec4b40e07debe98561e4",
     "url": "/abstrak/static/media/Recoleta-Regular.f6e3b6a9.woff2"
   },
   {
-    "revision": "5bd99be4d8c32a6517530c51d0f67e34",
-    "url": "/abstrak/static/media/Recoleta-Medium.5bd99be4.eot"
+    "revision": "7eb49619205b759292e7ec0a78a61e6c",
+    "url": "/abstrak/static/media/Recoleta-Regular.7eb49619.ttf"
+  },
+  {
+    "revision": "9711a8e7897d2d876b836ac797eabca8",
+    "url": "/abstrak/static/media/Recoleta-Regular.9711a8e7.eot"
+  },
+  {
+    "revision": "d22945b98eb940f2a798e625051c3e4d",
+    "url": "/abstrak/static/media/Recoleta-Medium.d22945b9.woff2"
   },
   {
     "revision": "c0906e2c9b8e59bf409107af5b067cec",
     "url": "/abstrak/static/media/Recoleta-Medium.c0906e2c.woff"
   },
   {
-    "revision": "b730951bf8361397041e17799be8e548",
-    "url": "/abstrak/static/media/Recoleta-Medium.b730951b.ttf"
+    "revision": "6aff230c15539d9aa16c3c8cc0f6be16",
+    "url": "/abstrak/static/media/Recoleta-Regular.6aff230c.woff"
   },
   {
-    "revision": "d22945b98eb940f2a798e625051c3e4d",
-    "url": "/abstrak/static/media/Recoleta-Medium.d22945b9.woff2"
+    "revision": "b730951bf8361397041e17799be8e548",
+    "url": "/abstrak/static/media/Recoleta-Medium.b730951b.ttf"
   },
   {
     "revision": "8ff060d3570f24be9f212b8a3b097a1f",
@@ -156,6 +152,10 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/Recoleta-SemiBold.cebc2687.woff2"
   },
   {
+    "revision": "89bb0a08e2daf8991360254fe0a367aa",
+    "url": "/abstrak/static/media/Recoleta-Bold.89bb0a08.eot"
+  },
+  {
     "revision": "c99924f2ced64a7062d3e8e0e1dcf46e",
     "url": "/abstrak/static/media/Recoleta-SemiBold.c99924f2.woff"
   },
@@ -164,52 +164,52 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/Recoleta-SemiBold.3a626358.ttf"
   },
   {
-    "revision": "89bb0a08e2daf8991360254fe0a367aa",
-    "url": "/abstrak/static/media/Recoleta-Bold.89bb0a08.eot"
-  },
-  {
-    "revision": "c57966adabf72b655832eaf0dbbac94b",
-    "url": "/abstrak/static/media/Recoleta-Bold.c57966ad.ttf"
+    "revision": "a2650163d99c2023bd83cace336ca2ac",
+    "url": "/abstrak/static/media/Recoleta-Bold.a2650163.woff"
   },
   {
     "revision": "42a4e04a7fadeb8fbf94af1fd5787f0e",
     "url": "/abstrak/static/media/Recoleta-Bold.42a4e04a.woff2"
   },
   {
+    "revision": "5bd99be4d8c32a6517530c51d0f67e34",
+    "url": "/abstrak/static/media/Recoleta-Medium.5bd99be4.eot"
+  },
+  {
     "revision": "e7f5aa5e50f27efcfc53af13d82fb582",
     "url": "/abstrak/static/media/Recoleta-Black.e7f5aa5e.eot"
-  },
-  {
-    "revision": "a2650163d99c2023bd83cace336ca2ac",
-    "url": "/abstrak/static/media/Recoleta-Bold.a2650163.woff"
-  },
-  {
-    "revision": "5aa019c2bde207272074cf46037f0f4c",
-    "url": "/abstrak/static/media/Recoleta-Black.5aa019c2.woff"
   },
   {
     "revision": "94bcced9bc9c91a56653512324b29d6a",
     "url": "/abstrak/static/media/Recoleta-Black.94bcced9.ttf"
   },
   {
-    "revision": "82bec4d8ba35ab089deefb9a7a4bd78e",
-    "url": "/abstrak/static/media/Recoleta-Black.82bec4d8.woff2"
+    "revision": "9bd1e23e87cacceb11aee3f04f8535c4",
+    "url": "/abstrak/static/media/Flaticon.9bd1e23e.eot"
   },
   {
-    "revision": "11eaaa2bca269e4accc4e3db0cec3302",
-    "url": "/abstrak/static/media/Flaticon.11eaaa2b.woff"
+    "revision": "82bec4d8ba35ab089deefb9a7a4bd78e",
+    "url": "/abstrak/static/media/Recoleta-Black.82bec4d8.woff2"
   },
   {
     "revision": "b3ce6653fa3521304790c463a9ad1b05",
     "url": "/abstrak/static/media/Flaticon.b3ce6653.woff2"
   },
   {
-    "revision": "02b61faa7e53808450d11c073065b060",
-    "url": "/abstrak/static/media/Flaticon.02b61faa.ttf"
+    "revision": "c57966adabf72b655832eaf0dbbac94b",
+    "url": "/abstrak/static/media/Recoleta-Bold.c57966ad.ttf"
   },
   {
-    "revision": "9bd1e23e87cacceb11aee3f04f8535c4",
-    "url": "/abstrak/static/media/Flaticon.9bd1e23e.eot"
+    "revision": "11eaaa2bca269e4accc4e3db0cec3302",
+    "url": "/abstrak/static/media/Flaticon.11eaaa2b.woff"
+  },
+  {
+    "revision": "5aa019c2bde207272074cf46037f0f4c",
+    "url": "/abstrak/static/media/Recoleta-Black.5aa019c2.woff"
+  },
+  {
+    "revision": "02b61faa7e53808450d11c073065b060",
+    "url": "/abstrak/static/media/Flaticon.02b61faa.ttf"
   },
   {
     "revision": "e26be2c2e576088ff7745f06cfaa1fc6",
@@ -224,10 +224,6 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/abstrak/static/media/fontawesome-webfont.fee66e71.woff"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/abstrak/static/media/fontawesome-webfont.b06871f2.ttf"
   },
@@ -236,24 +232,24 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/abstrak/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
     "revision": "f0d428213155f923352503d7929250c4",
     "url": "/abstrak/static/media/flaticon.f0d42821.ttf"
   },
   {
-    "revision": "775693e559075fe6d8cd1f8479c34346",
-    "url": "/abstrak/static/media/bg-03.775693e5.jpg"
+    "revision": "8f1916f6efdeef4f61e81dd8d76aee7b",
+    "url": "/abstrak/static/media/bg-04.8f1916f6.jpg"
   },
   {
     "revision": "b341a68355d4e4467c1df7415fa76f3d",
     "url": "/abstrak/static/media/bg-01.b341a683.jpg"
   },
   {
-    "revision": "597494974fbdd55a509e785b8f6e196a",
-    "url": "/abstrak/static/media/bg-02.59749497.jpg"
-  },
-  {
-    "revision": "8f1916f6efdeef4f61e81dd8d76aee7b",
-    "url": "/abstrak/static/media/bg-04.8f1916f6.jpg"
+    "revision": "775693e559075fe6d8cd1f8479c34346",
+    "url": "/abstrak/static/media/bg-03.775693e5.jpg"
   },
   {
     "revision": "8f01a4f25a8a9e18d401420e38b772dc",
@@ -268,16 +264,12 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/hero-3.59749497.jpg"
   },
   {
-    "revision": "e15f32581ee691822eb249098e6d24c8",
-    "url": "/abstrak/static/media/hero-4.e15f3258.jpg"
-  },
-  {
     "revision": "1f2ebd489ddf3e04289bffe91a69af8f",
     "url": "/abstrak/static/media/hero-7.1f2ebd48.jpg"
   },
   {
-    "revision": "af9addd30fc137e46fce5167c0697b56",
-    "url": "/abstrak/static/media/hero-8.af9addd3.jpg"
+    "revision": "e15f32581ee691822eb249098e6d24c8",
+    "url": "/abstrak/static/media/hero-4.e15f3258.jpg"
   },
   {
     "revision": "03fa2d52fae5ad59a3cfba0193c2d477",
@@ -288,32 +280,40 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/hero-9.8536578d.jpg"
   },
   {
+    "revision": "af9addd30fc137e46fce5167c0697b56",
+    "url": "/abstrak/static/media/hero-8.af9addd3.jpg"
+  },
+  {
+    "revision": "597494974fbdd55a509e785b8f6e196a",
+    "url": "/abstrak/static/media/bg-02.59749497.jpg"
+  },
+  {
     "revision": "02e5094a141f070e9239068f5473baed",
     "url": "/abstrak/static/media/hero-14.02e5094a.jpg"
-  },
-  {
-    "revision": "8f1916f6efdeef4f61e81dd8d76aee7b",
-    "url": "/abstrak/static/media/hero-15.8f1916f6.jpg"
-  },
-  {
-    "revision": "3ef35b18d39994e1def3cb63feb3c805",
-    "url": "/abstrak/static/media/hero-19.3ef35b18.jpg"
-  },
-  {
-    "revision": "7223318c6862d0130bf5c899373362ac",
-    "url": "/abstrak/static/media/hero-23.7223318c.jpg"
-  },
-  {
-    "revision": "e67020c8a417ffbb463444270c7d9cae",
-    "url": "/abstrak/static/media/hero-17.e67020c8.jpg"
   },
   {
     "revision": "ad9a464631894d5cba1e1df4ff9cec1b",
     "url": "/abstrak/static/media/hero-13.ad9a4646.jpg"
   },
   {
+    "revision": "7223318c6862d0130bf5c899373362ac",
+    "url": "/abstrak/static/media/hero-23.7223318c.jpg"
+  },
+  {
     "revision": "513d0f98444b916743b7f9662c94838f",
     "url": "/abstrak/static/media/hero-20.513d0f98.jpg"
+  },
+  {
+    "revision": "3ef35b18d39994e1def3cb63feb3c805",
+    "url": "/abstrak/static/media/hero-19.3ef35b18.jpg"
+  },
+  {
+    "revision": "8f1916f6efdeef4f61e81dd8d76aee7b",
+    "url": "/abstrak/static/media/hero-15.8f1916f6.jpg"
+  },
+  {
+    "revision": "e67020c8a417ffbb463444270c7d9cae",
+    "url": "/abstrak/static/media/hero-17.e67020c8.jpg"
   },
   {
     "revision": "d2b3adbdbe63412960a3e3853bf5f85b",
@@ -324,7 +324,7 @@ self.__precacheManifest = [
     "url": "/abstrak/static/media/download-page.b4c60e90.jpg"
   },
   {
-    "revision": "3ae66749327d182a9defc666974682d4",
+    "revision": "d098ced6ae79f5743a7d5e1cf35ba034",
     "url": "/abstrak/index.html"
   }
 ];
